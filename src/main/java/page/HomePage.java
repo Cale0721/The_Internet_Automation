e45e3a20297e87c4c3da92074f89b51b356d70e5
Caleb Moore <cale0721@gmail.com>
2023-09-07 14:46:51 -0400

    navigation of windows

--- src/main/java/page/HomePage.java
+++ src/main/java/page/HomePage.java
@@ -82,6 +82,13 @@ public class HomePage{
         return new LargeAndDeepDomPage(driver);
      }
 
+    public MultipleWindowsPage clickMultipleWindows(){
+        clickLink("Multiple Windows");
+        return new MultipleWindowsPage(driver);
+    }
+
+
+
         private void clickLink(String linkText){
         driver.findElement(By.linkText(linkText)).click();
         }
